e21fa39c3643cce457624f637aafd3fa3795024b
Amanda Pettenati <amanda@pette.net>
2021-06-07 15:28:40 -0400

    initialize project

---
+++ src/app.ts
@@ -0,0 +1,8 @@
+import express from 'express'
+
+const app = express()
+const port = 3000
+
+app.get('/', (_, res) => res.send('hello world'))
+
+app.listen(port, () => { console.log(`Server running on port ${port}`) })
