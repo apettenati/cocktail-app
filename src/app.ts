4330160c99541ffafd41be5fa9753f2a075f93a6
Amanda Pettenati <amanda@pette.net>
2021-06-07 19:38:30 -0400

    add mongoose connection

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,27 @@
 import express from 'express'
-import { IndexRouter } from './routes/index'
-import { IngredientsRouter } from './routes/ingredients'
+import cors from 'cors'
 
+// Establish app
 const app = express()
 const port = 3000
 
+// Establish middleware
+app.use(cors())
 app.use(express.json())
 
+// Establish mongodb connection
+import 'dotenv/config'
+// FIXME URI not working
+const uri = process.env['ATLAS_URI']
+import mongoose from 'mongoose'
+mongoose.connect(`${uri}`, { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+db.once('open', () => console.log('MongoDB database connection established successfully'))
+
+// Establish Routers
+import { IndexRouter } from './routes/index'
+import { IngredientsRouter } from './routes/ingredients'
 app.use('/', IndexRouter)
 app.use('/ingredients', IngredientsRouter)
 
