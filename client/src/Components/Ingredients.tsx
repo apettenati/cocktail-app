009dd8a7797f9d5be65f7fd6c1bebe6d9bd7a456
Amanda Pettenati <amanda@pette.net>
2021-06-11 13:36:16 -0400

    add ingredients checkboxes

--- client/src/Components/Ingredients.tsx
+++ client/src/Components/Ingredients.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import { useState } from 'react';
 import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
 import FormLabel from '@material-ui/core/FormLabel';
 import FormControl from '@material-ui/core/FormControl';
@@ -26,50 +26,34 @@ interface IngredientsProps {
 
 export const Ingredients: React.FC<IngredientsProps> = ({ authenticated }) => {
   const classes = useStyles();
-  const [state, setState] = React.useState({
-    gilad: true,
-    jason: false,
-    antoine: false,
-  });
+  const [userIngredients, setUserIngredients] = useState([])
+
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setState({ ...state, [event.target.name]: event.target.checked });
+    setUserIngredients({ ...userIngredients, [event.target.name]: event.target.checked });
   };
 
-  const { gilad, jason, antoine } = state;
 
   return (
     <div className={classes.root}>
-      {!authenticated
-        ? <p>Please log in to view this page</p>
-        :
-        <FormControl component="fieldset" className={classes.formControl}>
-          <FormLabel component="legend">Select Ingredients You Have</FormLabel>
-          <FormGroup>
-            {INGREDIENTS.map((ingredient) => {
+      {/* {!authenticated */}
+      {/* ? <p>Please log in to view this page</p> */}
+      {/* : */}
+      <FormControl component="fieldset" className={classes.formControl}>
+        <FormLabel component="legend">Select Ingredients You Have</FormLabel>
+        <FormGroup>
+          {INGREDIENTS.map((ingredient) => {
+            return (
               <FormControlLabel
-                control={<Checkbox checked={gilad} onChange={handleChange} name={ingredient} />}
+                control={<Checkbox checked={false} onChange={handleChange} name={ingredient} />}
                 label={ingredient}
               />
-
-            })}
-            <FormControlLabel
-              control={<Checkbox checked={gilad} onChange={handleChange} name="gilad" />}
-              label="Gilad Gray"
-            />
-            <FormControlLabel
-              control={<Checkbox checked={jason} onChange={handleChange} name="jason" />}
-              label="Jason Killian"
-            />
-            <FormControlLabel
-              control={<Checkbox checked={antoine} onChange={handleChange} name="antoine" />}
-              label="Antoine Llorca"
-            />
-          </FormGroup>
-          <FormHelperText>Be careful</FormHelperText>
-        </FormControl>
-      }
+            )
+          })}
+        </FormGroup>
+        {/* <FormHelperText>Be careful</FormHelperText> */}
+      </FormControl>
+      {/* } */}
     </div>
-
   );
 }
