e8a7b4b3734272c089033850a9c9e2c28f913b93
Amanda Pettenati <amanda@pette.net>
2021-06-15 20:52:51 -0400

    add logout logic

--- client/src/Components/User/Logout.tsx
+++ client/src/Components/User/Logout.tsx
@@ -1,12 +1,58 @@
-interface LogoutProps {
-  setAuthenticated: (authenticated: boolean) => void
-}
+import { useHistory } from 'react-router-dom'
+import Button from '@material-ui/core/Button'
+import { makeStyles } from '@material-ui/core/styles'
+import { UserStore } from '../../store'
 
-export const Logout: React.FC<LogoutProps> = ({ setAuthenticated }) => {
+const useStyles = makeStyles((theme) => ({
+  '@global': {
+    ul: {
+      margin: 0,
+      padding: 0,
+      listStyle: 'none',
+    },
+  },
+  appBar: {
+    // borderBottom: `1px solid ${theme.palette.divider}`,
+  },
+  toolbar: {
+    flexWrap: 'wrap',
+  },
+  toolbarTitle: {
+    flexGrow: 1,
+  },
+  link: {
+    margin: theme.spacing(1, 1.5),
+  },
+}))
+
+export const Logout = () => {
+  const classes = useStyles()
+  const history = useHistory()
+
+  function logout(event: React.FormEvent<HTMLButtonElement>) {
+    event?.preventDefault()
+    fetch('http://localhost:4000/user/logout', {
+      method: "POST",
+      credentials: "include",
+      headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Credentials": "true"
+      },
+    }).then((res) => {
+      console.log('success login')
+      window.localStorage.removeItem('username')
+      UserStore.update((s) => { s.authenticated = false })
+      UserStore.update((s) => { s.username = '' })
+      history.push('/login')
+      console.log('logged out')
+    })
+      .catch((error) => { console.log({ error }) })
+  }
+
   return (
-    <div className="logout">
-      <h1> Logout</h1>
-
-    </div>
+    <Button href="/user/logout" onClick={logout} color="primary" variant="outlined" className={classes.link}>
+      Logout
+    </Button>
   )
 }
